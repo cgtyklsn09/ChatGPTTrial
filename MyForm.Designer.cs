d4a3b09ccdbab71d08941532ef1f91eccd8339fe
Çağatay Kalsan <cagatay.kalsan@codeo.com.tr>
2023-01-13 16:35:16 +0300

    v.0.1.0

--- MyForm.Designer.cs
+++ MyForm.Designer.cs
@@ -28,13 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(645, 20);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(12, 47);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(776, 391);
+            this.outputTextBox.TabIndex = 1;
+            this.outputTextBox.Text = "";
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(663, 12);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(125, 23);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "MyForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.Button submitButton;
     }
 }
 
--- MyForm.cs
+++ MyForm.cs
@@ -8,60 +8,73 @@ namespace ChatGPTTrial2
 {
     public partial class MyForm : Form
     {
-        private TextBox inputTextBox;
-        private TextBox outputTextBox;
-        private Button submitButton;
 
         // Replace YOUR_API_KEY with your actual API key
         const string API_KEY = "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";
         public MyForm()
         {
             InitializeComponent();
-            inputTextBox = new TextBox();
-            outputTextBox = new TextBox();
-            submitButton = new Button();
-
-            inputTextBox.Location = new System.Drawing.Point(10, 10);
-            inputTextBox.Size = new System.Drawing.Size(200, 20);
-
-            outputTextBox.Location = new System.Drawing.Point(10, 50);
-            outputTextBox.Size = new System.Drawing.Size(200, 20);
-            outputTextBox.ReadOnly = true;
-
-            submitButton.Location = new System.Drawing.Point(10, 90);
-            submitButton.Size = new System.Drawing.Size(75, 23);
-            submitButton.Text = "Submit";
-            submitButton.Click += new EventHandler(this.submitButton_Click);
-
-            this.Controls.Add(inputTextBox);
-            this.Controls.Add(outputTextBox);
-            this.Controls.Add(submitButton);
         }
+
+        //private async void submitButton_Click(object sender, EventArgs e)
+        //{
+        //    using (var client = new HttpClient())
+        //    {
+        //        // Set the API key in the request headers
+        //        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + API_KEY);
+
+        //        // Set the request parameters
+        //        var request = new HttpRequestMessage(HttpMethod.Post, "https://api.openai.com/v1/engines/davinci-codex/completions");
+        //        request.Content = new StringContent("{\"prompt\":\"" + inputTextBox.Text + "\", \"temperature\":0.5}", Encoding.UTF8, "application/json");
+
+        //        // Send the request
+        //        var response = await client.SendAsync(request);
+
+        //        // Get the response
+        //        var responseString = await response.Content.ReadAsStringAsync();
+
+        //        // Deserialize the response
+        //        dynamic json = JsonConvert.DeserializeObject(responseString);
+
+        //        // Display the response
+        //        outputTextBox.Text = json.choices[0].text;
+        //    }
+        //}
 
         private async void submitButton_Click(object sender, EventArgs e)
         {
-            using (var client = new HttpClient())
-            {
-                // Set the API key in the request headers
-                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + API_KEY);
+            HttpClient client = new HttpClient();
 
-                // Set the request parameters
-                var request = new HttpRequestMessage(HttpMethod.Post, "https://api.openai.com/v1/engines/davinci-codex/completions");
-                request.Content = new StringContent("{\"prompt\":\"" + inputTextBox.Text + "\", \"temperature\":0.5}", Encoding.UTF8, "application/json");
+            client.DefaultRequestHeaders.Add("authorization", "Bearer " + API_KEY);
 
-                // Send the request
-                var response = await client.SendAsync(request);
+            var content = new StringContent("{\"model\": \"text-davinci-001\", \"prompt\": \"" + inputTextBox.Text + "\",\"temperature\": 1,\"max_tokens\": 100}",
+    Encoding.UTF8, "application/json");
 
-                // Get the response
-                var responseString = await response.Content.ReadAsStringAsync();
+            HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
 
-                // Deserialize the response
-                dynamic json = JsonConvert.DeserializeObject(responseString);
+            string responseString = await response.Content.ReadAsStringAsync();
 
-                // Display the response
-                outputTextBox.Text = json.choices[0].text;
-            }
+            var dyData = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+            string guess = dyData.choices[0].text;
+            outputTextBox.Text = guess.Substring(2);
+        }
+
+        static string GuessCommand(string raw)
+        {
+            Console.WriteLine("---> GPT-3 API Returned Text:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(raw);
+
+            var lastIndex = raw.LastIndexOf('\n');
+
+            string guess = raw.Substring(lastIndex + 1);
+
+            Console.ResetColor();
+
+            TextCopy.ClipboardService.SetText(guess);
+
+            return guess;
         }
     }
-
 }
