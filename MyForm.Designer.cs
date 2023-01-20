5f23e98c73073d9f7aa48107664507aa1f442481
kivanckizildemir <48845235+kivanckizildemir@users.noreply.github.com>
2023-01-20 11:39:07 +0300

    ChatGPT + CSharpProvider + MS Speech Recog

--- MyForm.Designer.cs
+++ MyForm.Designer.cs
@@ -31,26 +31,28 @@
             this.inputTextBox = new System.Windows.Forms.TextBox();
             this.outputTextBox = new System.Windows.Forms.RichTextBox();
             this.submitButton = new System.Windows.Forms.Button();
+            this.startStopListeningButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // inputTextBox
             // 
             this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Multiline = true;
             this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.Size = new System.Drawing.Size(645, 20);
+            this.inputTextBox.Size = new System.Drawing.Size(645, 54);
             this.inputTextBox.TabIndex = 0;
             // 
             // outputTextBox
             // 
-            this.outputTextBox.Location = new System.Drawing.Point(12, 47);
+            this.outputTextBox.Location = new System.Drawing.Point(12, 72);
             this.outputTextBox.Name = "outputTextBox";
-            this.outputTextBox.Size = new System.Drawing.Size(776, 391);
+            this.outputTextBox.Size = new System.Drawing.Size(776, 366);
             this.outputTextBox.TabIndex = 1;
             this.outputTextBox.Text = "";
             // 
             // submitButton
             // 
-            this.submitButton.Location = new System.Drawing.Point(663, 12);
+            this.submitButton.Location = new System.Drawing.Point(663, 43);
             this.submitButton.Name = "submitButton";
             this.submitButton.Size = new System.Drawing.Size(125, 23);
             this.submitButton.TabIndex = 2;
@@ -58,11 +60,22 @@
             this.submitButton.UseVisualStyleBackColor = true;
             this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
             // 
+            // startStopListeningButton
+            // 
+            this.startStopListeningButton.Location = new System.Drawing.Point(663, 10);
+            this.startStopListeningButton.Name = "startStopListeningButton";
+            this.startStopListeningButton.Size = new System.Drawing.Size(125, 23);
+            this.startStopListeningButton.TabIndex = 3;
+            this.startStopListeningButton.Text = "Start Listening";
+            this.startStopListeningButton.UseVisualStyleBackColor = true;
+            this.startStopListeningButton.Click += new System.EventHandler(this.startStopListeningButton_Click);
+            // 
             // MyForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startStopListeningButton);
             this.Controls.Add(this.submitButton);
             this.Controls.Add(this.outputTextBox);
             this.Controls.Add(this.inputTextBox);
@@ -78,6 +91,7 @@
         private System.Windows.Forms.TextBox inputTextBox;
         private System.Windows.Forms.RichTextBox outputTextBox;
         private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button startStopListeningButton;
     }
 }
 
--- MyForm.cs
+++ MyForm.cs
@@ -3,31 +3,102 @@ using System.Windows.Forms;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Text;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using System.IO;
+using System.Speech.Recognition;
+
 
 namespace ChatGPTTrial2
 {
     public partial class MyForm : Form
     {
-        const string API_KEY = "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";
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        // Replace YOUR_API_KEY with your actual API key
+        const string API_KEY = "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";
+
+        bool _isListening = false;
         public MyForm()
         {
             InitializeComponent();
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammar(new DictationGrammar());
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(_recognizer_SpeechRecognized);
+        }
+
+        private void _recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            inputTextBox.Invoke(new Action(() => inputTextBox.Text = e.Result.Text));
+        }
+
+        private void startStopListeningButton_Click(object sender, EventArgs e)
+        {
+            if (_isListening)
+            {
+                _recognizer.RecognizeAsyncStop();
+                startStopListeningButton.Text = "Start Listening";
+                _isListening = false;
+            }
+            else
+            {
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                startStopListeningButton.Text = "Stop Listening";
+                _isListening = true;
+            }
+
         }
 
         private async void submitButton_Click(object sender, EventArgs e)
         {
             HttpClient client = new HttpClient();
+
             client.DefaultRequestHeaders.Add("authorization", "Bearer " + API_KEY);
 
-            var content = new StringContent("{\"model\": \"text-davinci-003\", \"prompt\": \"" + inputTextBox.Text + "\",\"temperature\": 0.5,\"max_tokens\": 1000}",
-                        Encoding.UTF8, "application/json");
+            var content = new StringContent("{\"model\": \"text-davinci-003\", \"prompt\": \"" + "Please give me a standalone 'public static void Main()' method in C# to " + inputTextBox.Text + " in current Inventor window. Get an active instance of Inventor by using that:  " + "InvApp.txt" + ". Ignore code descriptions or instructions." + "\",\"temperature\": 0,\"max_tokens\": 1000}", Encoding.UTF8, "application/json");
 
             HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+
             string responseString = await response.Content.ReadAsStringAsync();
+
             var dyData = JsonConvert.DeserializeObject<dynamic>(responseString);
 
             string guess = dyData.choices[0].text;
             outputTextBox.Text = guess.Substring(2);
+
+            string code = guess.Substring(2);
+
+            string input = code;
+            int index = input.IndexOf("public static");
+            string output = input.Substring(index);
+
+
+            code = File.ReadAllText("Start.txt") + output + File.ReadAllText("End.txt");
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+            CompilerParameters parameters = new CompilerParameters();
+            parameters.GenerateExecutable = false;
+            parameters.GenerateInMemory = true;
+            parameters.ReferencedAssemblies.Add(@"C:\Program Files\Autodesk\Inventor 2022\Bin\Public Assemblies\Autodesk.Inventor.Interop.dll");
+            parameters.ReferencedAssemblies.Add(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.dll");
+            parameters.ReferencedAssemblies.Add(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.Windows.Forms.dll");
+            CompilerResults results = provider.CompileAssemblyFromSource(parameters, code);
+            if (results.Errors.HasErrors)
+            {
+                Console.WriteLine("Errors building the code:");
+                foreach (CompilerError error in results.Errors)
+                {
+                    Console.WriteLine(error.ToString());
+                    outputTextBox.Text = guess.Substring(2) + error.ToString();
+                }
+            }
+            else
+            {
+                Assembly assembly = results.CompiledAssembly;
+                Type[] types = assembly.GetTypes();
+                MethodInfo method = types[0].GetMethod("Main");
+                method.Invoke(null, null);
+
+            }
         }
 
         static string GuessCommand(string raw)
@@ -46,5 +117,6 @@ namespace ChatGPTTrial2
 
             return guess;
         }
+
     }
 }
